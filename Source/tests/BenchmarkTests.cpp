7b1fc986118306bc962bea019aa3b354a060a106
Simon Zimmermann <mail@simonzimmermann.de>
2024-11-20 15:19:27 +0100

    add CPU measurement (vanilla)

--- Source/tests/BenchmarkTests.cpp
+++ Source/tests/BenchmarkTests.cpp
@@ -1,10 +1,13 @@
+#include <chrono>
+
+#include <benchmark/benchmark.h>
+
 #include "../PluginTests.h"
 #include "../TestUtilities.h"
 
 struct BenchmarkTest : public PluginTest
 {
-    BenchmarkTest() : PluginTest ("Benchmark", 1,
-                                        {Requirements::Thread::backgroundThread, Requirements::GUI::requiresGUI}) {}
+    BenchmarkTest() : PluginTest ("Benchmark", 1, {Requirements::Thread::backgroundThread, Requirements::GUI::requiresGUI}) {}
 
     void runTest (PluginTests& ut, juce::AudioPluginInstance& instance) override
     {
@@ -28,23 +31,44 @@ struct BenchmarkTest : public PluginTest
 
         callPrepareToPlayOnMessageThreadIfVST3 (instance, sampleRate, blockSize);
 
+        struct timespec start, end;
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+        uint64_t totalCPUTime = 0;
+        
         while (--numBlocks >= 0)
         {
-            /*for (auto parameter : parameters)
-            {
-                ut.logVerboseMessage (juce::String ("\nTesting parameter: ") + juce::String (parameter->getParameterIndex()) + " - " + parameter->getName (512));
-                for (auto parameter : parameters)
-                    parameter->setValue (r.nextFloat());
-            }
-            */
-
             fillNoise (ab);
+            
+            struct timespec blockStart, blockEnd;
+            clock_gettime(CLOCK_THREAD_CPUTIME_ID, &blockStart);
+            
             instance.processBlock (ab, mb);
+            
+            clock_gettime(CLOCK_THREAD_CPUTIME_ID, &blockEnd);
+            
+            // Calculate block CPU time in nanoseconds
+            uint64_t blockCPUTime = (blockEnd.tv_sec - blockStart.tv_sec) * 1000000000ULL 
+                                   + (blockEnd.tv_nsec - blockStart.tv_nsec);
+            totalCPUTime += blockCPUTime;
+            
             mb.clear();
-
             ut.resetTimeout();
             juce::Thread::sleep (10);
         }
+
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+
+        // Calculate total process CPU time
+        uint64_t processCPUTime = (end.tv_sec - start.tv_sec) * 1000000000ULL 
+                                 + (end.tv_nsec - start.tv_nsec);
+
+        // Calculate CPU usage percentage
+        double cpuUsagePercentage = (totalCPUTime * 100.0) / processCPUTime;
+
+        // Log results
+        ut.logMessage ("Total Process CPU Time: " + juce::String(processCPUTime) + " nanoseconds");
+        ut.logMessage ("Total Block CPU Time: " + juce::String(totalCPUTime) + " nanoseconds");
+        ut.logMessage ("Estimated CPU Usage: " + juce::String(cpuUsagePercentage) + "%");
     }
 };
 
